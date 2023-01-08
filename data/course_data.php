4efdc8816b555382aa79ea5cd9b51bd2480ef08d
Andrew Thwaites <andrewthwaites@hotmail.com>
2023-01-08 22:17:09 +0000

    Glossary and course data

---
+++ data/course_data.php
@@ -0,0 +1,30 @@
+<?php
+
+$course_data["CHAP01"] = "Planet Earth";
+$course_data["CHAP02"] = "Celestial observation";
+$course_data["CHAP03"] = "The lunar disc";
+$course_data["CHAP04"] = "Exploration of Moon";
+$course_data["CHAP05"] = "Exploring the Solar System";
+$course_data["CHAP06"] = "Solart System observartion";
+$course_data["CHAP07"] = "Early models of the Solar System";
+$course_data["CHAP08"] = "Planet Motion and gravity";
+$course_data["CHAP09"] = "Solar Astronomy";
+$course_data["CHAP10"] = "The Earth- Moon - Sun System";
+$course_data["CHAP11"] = "Time and the Earth, moon and the Sun";
+$course_data["CHAP12"] = "Formation of Planetary system";
+$course_data["CHAP13"] = "Exploring Starlight";
+$course_data["CHAP14"] = "Stellar Evolution"
+$course_data["CHAP15"] = "Our place in the galaxy";
+$course_data["CHAP16"] = "Cosmology";
+
+;
+
+
+
+
+
+
+
+
+
+
